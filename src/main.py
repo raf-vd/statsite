ebd6a8fed97b5c7673a3378380fafc691a2bbb22
rafvd <raf.van.dijck3@telenet.be>
2025-02-07 19:05:53 +0100

    blocks coded

--- src/main.py
+++ src/main.py
@@ -1,25 +1,128 @@
+import re
+import unittest
 from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, HTMLTag
 from leafnode import LeafNode
 from parentnode import ParentNode
 
+class TestFunction(unittest.TestCase):
+    def test_block_to_block_type(self):
+        # 1 #
+        txt = """# This is a heading
+This is a paragraph of text. It has some **bold** and *italic* words inside of it."""
+        self.assertEqual(block_to_block_type(txt), "heading")
+
+        # 2 #
+        txt ="""``` \w code block 
+some shitty code
+end of block```"""
+        self.assertEqual(block_to_block_type(txt), "code")
+
+        # 3 #
+        txt = """> quoted
+> text block    
+>"""    
+        # 4 #
+        self.assertEqual(block_to_block_type(txt), "quote")
+
+        txt = """* an example
+- of an unsorted list
+* even the - isn't a problem
+* """
+        # 5 #
+        self.assertEqual(block_to_block_type(txt), "unordered_list")
+
+        txt = """1. First line
+2. Second line
+3. Third line
+4. Fourth line"""    
+        self.assertEqual(block_to_block_type(txt), "ordered_list")
+
+        # 6 #
+        txt = "just some random text, so this should be a paragraph"
+        self.assertEqual(block_to_block_type(txt), "paragraph")
+
+#----------------------------------------------------------------------------------------#
+    def test_markdown_to_blocks(self):
+        # 1 #
+        txt = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+        self.assertListEqual(markdown_to_blocks(txt),['# This is a heading', 
+                                                     'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', 
+                                                     '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
+                                                    ])
+
+        # 2 #
+        txt = """
+        # This is a heading
+
+        
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+
+
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+
+
+"""
+        self.assertListEqual(markdown_to_blocks(txt),['# This is a heading', 
+                                                     'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', 
+                                                     '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
+                                                    ])
+
+#----------------------------------------------------------------------------------------#
+
+def markdown_to_blocks(markdown):
+    md_blocks = []
+    if markdown:
+        blocks = markdown.split("\n\n")
+        for block in blocks:
+            stripped = block.strip().strip("\n")
+            if stripped:
+                md_blocks.append(stripped)
+    return md_blocks
+
+
+def block_to_block_type(block_of_markdown):
+    
+    pattern = r"^#{1,6}"
+    if re.findall(pattern, block_of_markdown):return "heading"
+
+    pattern = r"^```[\s\S]*```$"
+    if re.findall(pattern, block_of_markdown):return "code"
+
+    pattern = r"^(>.*\n)*>.*$"
+    if re.findall(pattern, block_of_markdown):return "quote"
+
+    pattern = r"^([\*-] .*\n)*[\*-] .*$"
+    if re.findall(pattern, block_of_markdown):return "unordered_list"
+
+    # Last check, if this fails => default or paragraph
+    pattern = r"^(1\. .*\n|(\d+)\.\s.*\n)*\d\.\s.*$"    # Check for 'N. ' at the start of each line; including the last if empty"
+    if re.findall(pattern, block_of_markdown, re.MULTILINE):
+        lines = block_of_markdown.splitlines()          # Check for sequential numbers starting at 1
+        if lines:
+            for i, line in enumerate(lines, start=1):
+                if not re.match(rf"^{i}\. .+", line):  # Check if each line matches the number
+                    return "paragraph" 
+            return "ordered_list"
+    
+    return "paragraph"  # Failed all type checks => default to paragraph
+
 
 def main():
-    # node1 = LeafNode(None, "plain text?", None)
-    # node1 = LeafNode(HTMLTag.H1, "This is a nice header (one), isn't it?", None)
-    # node1 = LeafNode(HTMLTag.H5, "This is a nice header FIVE, isn't it?", None)
-    # node1 = LeafNode(HTMLTag.B, "Some bold text perhaps?", None)
-    # node1 = LeafNode(HTMLTag.A, "click me!", {"href": "https://www.google.com"})
-    # node1 = LeafNode(HTMLTag.IMG, " ", {"src":"public/kitten.jpg","alt":"cutie"})
-    
-    # tn = TextNode("just some text", TextType.TEXT)
-    # tn = TextNode("just some bold text", TextType.BOLD)
-    # tn = TextNode("just some italic text", TextType.ITALIC)
-    # tn = TextNode('print("just some code")', TextType.CODE)
-    # tn = TextNode("clickbait", TextType.LINK, "https://www.hln.be")
-    tn = TextNode("cutie", TextType.IMAGE, "image/kitten.jpg")
-    ln= text_node_to_html_node(tn)
-    print(ln.to_html())
+    pass
+
 
 main()
 
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 from leafnode import LeafNode
 from htmlnode import HTMLTag
@@ -23,8 +24,8 @@ class TextNode():
         return True
     
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-
+        return f'TextNode(text="{self.text}", text_type="{self.text_type.value}", url="{self.url}")'
+        # return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 def text_node_to_html_node(text_node):
     if not text_node.text_type in TextType:
@@ -45,3 +46,61 @@ def text_node_to_html_node(text_node):
             return LeafNode(HTMLTag.IMG, "", {"src": text_node.url,"alt": text_node.text})
         case _:
             raise Exception("This is not a convertible TextNode type")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if not old_nodes: return []
+
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT: 
+            new_nodes.append(node)            
+        else:
+            parts = node.text.split(delimiter, 2)
+            if len(parts) == 1:
+                new_nodes.append(node)
+            elif len(parts) == 2:
+                raise Exception("The node doesn't contain a valid Markdown syntax")
+            else:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                new_nodes.append(TextNode(parts[1], text_type))
+                new_nodes.extend(split_nodes_delimiter([TextNode(parts[2], TextType.TEXT)], delimiter, text_type))
+                # new_nodes.append(TextNode(parts[2], TextType.TEXT))
+    return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_link(old_nodes, img=""):
+    new_nodes = []                                                                  # Create empty list to add to
+    for node in old_nodes:                                                          # Loop across all input nodes
+        if node.text_type != TextType.TEXT:                                         # If not a TEXT noden just add to list
+            new_nodes.append(node)
+        else:
+            if img: links = extract_markdown_images(node.text)                      # Extract links from the node
+            else: links = extract_markdown_links(node.text)                         # Extract links from the node
+            for link in links:                                                      # Loop all found links
+                parts = node.text.split(f"{img}[{link[0]}]({link[1]})", 1)          # Find whatever is before the link
+                if parts[0] != "":                                              
+                    new_nodes.append(TextNode(parts[0], TextType.TEXT))             # Append whatever is before link to new list
+                    node.text = node.text.replace(f"{parts[0]}", "")                # Remove the just added text from the original node text                    
+                new_nodes.append(TextNode(link[0], TextType.IMAGE if img else TextType.LINK, link[1])) # Append the image/link itself as a (LINK) node
+                node.text = node.text.replace(f"{img}[{link[0]}]({link[1]})", "")   # Remove the just added link from the original node text
+            if node.text != '':                                                     # Don't do anything if nothing after final link
+                new_nodes.append(TextNode(node.text, TextType.TEXT))                # Apend the text after final link 
+    return new_nodes                                                                # Return the list
+
+def split_nodes_image(old_nodes):                                                   # Call code for link with optional parameter (! to indicate image)
+    return split_nodes_link(old_nodes, "!")                                         # Return the list
+
+def text_to_textnodes(text):
+    nodes = []
+    nodes.append(TextNode(text, TextType.TEXT))
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
